280fdf7a290def22cb925a85b8ce167b0acf9b99
Fabrice-64 <fab.jaouen@gmail.com>
2021-03-26 17:57:25 +0100

    Customize UserModel

--- advocacy_project/settings.py
+++ advocacy_project/settings.py
@@ -46,6 +46,8 @@ INSTALLED_APPS = [
     'pages',
 ]
 
+AUTH_USER_MODEL = 'accounts.CustomUser'
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
