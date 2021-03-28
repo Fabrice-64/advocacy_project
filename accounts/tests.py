8924ee1cec2a6dc8eddc965ade0e80d86479dd49
Fabrice-64 <fab.jaouen@gmail.com>
2021-03-28 15:04:46 +0200

    Implement HomePage View and Tests

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,3 +1,28 @@
 from django.test import TestCase
-
+from django.contrib.auth import get_user_model
 # Create your tests here.
+
+class CustomUserTest(TestCase):
+    def test_create_user(self):
+        User = get_user_model()
+        user = User.objects.create_user(
+            username="Jean",
+            email="jean@test.com",
+            password="test1234"
+        )
+        self.assertEqual(user.username, "Jean")
+        self.assertEqual(user.email, "jean@test.com")
+        self.assertFalse(user.is_staff)
+        self.assertFalse(user.is_superuser)
+
+    def test_create_superuser(self):
+        User = get_user_model()
+        admin_user = User.objects.create_superuser(
+            username="Paul",
+            email="paul@test.com",
+            password="test1234"
+        )
+        self.assertEqual(admin_user.username, "Paul")
+        self.assertEqual(admin_user.email, "paul@test.com")
+        self.assertTrue(admin_user.is_staff)
+        self.assertTrue(admin_user.is_superuser)
--- accounts/urls.py
+++ accounts/urls.py
@@ -7,5 +7,7 @@ urlpatterns = [
     path('login/', views.LoginView.as_view(
         template_name='registration/login.html'
     ), name='login'),
-    path('logout/', views.LogoutView.as_view(), name='logout'),
+    path('logout/', views.LogoutView.as_view(
+        template_name='registration/logout.html'
+    ), name='logout'),
 ]
