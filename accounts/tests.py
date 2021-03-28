5db0c000394b999a5660c499c59468c56d2d4863
Fabrice-64 <fab.jaouen@gmail.com>
2021-03-28 17:59:36 +0200

    Implement Create User + tests

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
+from django.urls import reverse
 # Create your tests here.
 
 class CustomUserTest(TestCase):
@@ -26,3 +27,35 @@ class CustomUserTest(TestCase):
         self.assertEqual(admin_user.email, "paul@test.com")
         self.assertTrue(admin_user.is_staff)
         self.assertTrue(admin_user.is_superuser)
+
+class SignUpPageTest(TestCase):
+
+    def setUp(self):
+        url = reverse('accounts:signup')
+        self.response = self.client.get(url)
+
+    def test_signup_template(self):
+        self.assertEqual(self.response.status_code, 200)
+        self.assertTemplateUsed(self.response, 'registration/signup.html')
+        self.assertContains(self.response, 'Création de Compte')
+
+class LoginTest(TestCase):
+
+    def setUp(self):
+        url = reverse('accounts:login')
+        self.response = self.client.get(url)
+
+    def test_login(self):
+        self.assertEqual(self.response.status_code, 200)
+        self.assertTemplateUsed(self.response, 'registration/login.html')
+        self.assertContains(self.response, 'Connexion')
+
+
+class LogoutTest(TestCase):
+    def setUp(self):
+        url = reverse('accounts:logout')
+        self.response = self.client.get(url)
+
+    def test_logout(self):
+        # status_code is 302 as logout redirects to homepage iaw the settings.
+        self.assertEqual(self.response.status_code, 302)
--- accounts/urls.py
+++ accounts/urls.py
@@ -1,5 +1,6 @@
 from django.contrib.auth import views
 from django.urls import path
+from .views import SignUpPageView
 
 app_name =  'accounts'
 
@@ -10,4 +11,7 @@ urlpatterns = [
     path('logout/', views.LogoutView.as_view(
         template_name='registration/logout.html'
     ), name='logout'),
+    path('signup/', SignUpPageView.as_view(
+        template_name='registration/signup.html'
+        ), name='signup'),
 ]
