deb4ff2eb7a3b34315e55f5fd19f554f929163a6
Fabrice-64 <fab.jaouen@gmail.com>
2021-04-07 17:54:41 +0200

    Tests HomePage OK.

--- pages/tests.py
+++ pages/tests.py
@@ -1,24 +1,22 @@
 from django.test import SimpleTestCase
-from django.urls import reverse, resolve
+from django.urls import reverse_lazy, resolve
 from .views import HomePageView
 
 # Create your tests here.
 class HomePageTest(SimpleTestCase):
 
     def setUp(self):
-        url = reverse('home')
+        url = reverse_lazy('pages:home')
         self.response = self.client.get(url)
 
     def test_homepage_status(self):
         self.assertEqual(self.response.status_code, 200)
         self.assertTemplateUsed(self.response, 'home.html')
-        self.assertContains(self.response, "Bienvenue sur la page d'accueil")
+        self.assertContains(self.response, "Bienvenue Sur notre Application de Plaidoyer")
         self.assertNotContains(self.response, "<h1>Connexion<h1>")
-
-    def test_homepage_url(self):
-        self.assertEqual(self.response.status_code, 200)
-
+        
     def test_homepage_resolves_homepage_view(self):
         view = resolve('/')
         self.assertEqual(view.func.__name__,
             HomePageView.as_view().__name__)
+    
