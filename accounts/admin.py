a2f21e730fa5f2cea3b45130a55a2b4d5689d653
Fabrice-64 <fab.jaouen@gmail.com>
2021-04-20 19:24:45 +0200

    Customize CustomUserAdmin for User creation

--- accounts/admin.py
+++ accounts/admin.py
@@ -14,10 +14,11 @@ class CustomUserAdmin(UserAdmin):
     list_display = ('first_name', 'last_name', 'email', 'username', 'phone_number', 'team', 'status_type')
 
     fieldsets = UserAdmin.fieldsets + (
-        (None, {'fields': ('phone_number', 'team', 'status_type',)}),
+        ('Coordonnées', {'fields': ('phone_number',)}),
+        ('Fonction', {'fields': ('team', 'status_type')}),
     )
     add_fieldsets = UserAdmin.add_fieldsets + (
-        (None, {'fields':('phone_number', 'team', 'status_type',)}),
+        ('Coordonnées', {'fields': ('phone_number',)}),
+        ('Fonction', {'fields': ('team', 'status_type')}),
     )
-
 admin.site.register(CustomUser, CustomUserAdmin)
