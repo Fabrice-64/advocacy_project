6ca6269fc282a96a43e894038f25f9dc1c6b670c
Fabrice-64 <fab.jaouen@gmail.com>
2021-06-01 15:16:39 +0200

    Select officials propinquity & influence ( pandas)

---
+++ officials/ranking_statistics.py
@@ -0,0 +1,33 @@
+import pandas as pd
+
+class GetStatsFromOfficials:
+    def __init__(self, officials):
+        self.officials = pd.DataFrame(officials)
+
+    def get_stats_from_officials(self):
+        officials_description = self.officials.describe()
+        return officials_description.to_dict('dict')
+
+    def get_quantiles_from_officials(self):
+        officials_quantiles = self.officials.quantile([0.25, 0.5, 0.75, 1])
+        return officials_quantiles.to_dict('dict')
+
+    def get_officials_below_P50_I50(self):
+        return self.officials.loc[(
+                self.officials['propinquity'] < self.officials['propinquity'].quantile(0.50)) & (
+                self.officials['influence'] <self.officials['influence'].quantile(0.50))].to_dict('index')
+    
+    def get_officials_below_P50_above_I50(self):
+        return self.officials.loc[(
+                self.officials['propinquity'] < self.officials['propinquity'].quantile(0.50)) & (
+                self.officials['influence'] >= self.officials['influence'].quantile(0.50))].to_dict('index')
+    
+    def get_officials_above_P50_below_I50(self):
+        return self.officials.loc[(
+                self.officials['propinquity'] >= self.officials['propinquity'].quantile(0.50)) & (
+                self.officials['influence'] < self.officials['influence'].quantile(0.50))].to_dict('index')
+
+    def get_officials_above_P50_above_I50(self):
+        return self.officials.loc[(
+                self.officials['propinquity'] >= self.officials['propinquity'].quantile(0.50)) & (
+                self.officials['influence'] >= self.officials['influence'].quantile(0.50))].to_dict('index')
---
+++ officials/tests/test_ranking_statistics.py
@@ -0,0 +1,33 @@
+from django.test import TestCase
+import officials.ranking_statistics as rs
+from .database_sample import ranking_sample
+
+class RankingStatisticsTest(TestCase):
+
+    def setUp(self):
+        self.officials_statistics = rs.GetStatsFromOfficials(ranking_sample)
+
+    def test_officials_statistics(self):
+        officials_statistics = self.officials_statistics.get_stats_from_officials()
+        self.assertEqual(len(officials_statistics), 3)
+        self.assertEqual(officials_statistics['propinquity']['count'], 20)
+
+    def test_get_quantiles_from_officials(self):
+        officials_quantiles = self.officials_statistics.get_quantiles_from_officials()
+        self.assertEqual(officials_quantiles['propinquity'][0.5], 6.15)
+
+    def test_get_officials_below_P50_I50(self):
+        officials_below_50_50 = self.officials_statistics.get_officials_below_P50_I50()
+        self.assertEqual(len(officials_below_50_50), 3)
+    
+    def test_get_officials_below_P50_above_I50(self):
+        officials_sample = self.officials_statistics.get_officials_below_P50_above_I50()
+        self.assertEqual(len(officials_sample), 7)
+    
+    def test_get_officials_above_P50_below_I50(self):
+        officials_sample = self.officials_statistics.get_officials_above_P50_below_I50()
+        self.assertEqual(len(officials_sample), 6)
+    
+    def test_get_officials_above_P50_above_I50(self):
+        officials_sample = self.officials_statistics.get_officials_above_P50_above_I50()
+        self.assertEqual(len(officials_sample), 4)
