0abc1c5aab58bbf0ca9af3380454ac8af9f8d4de
Fabrice-64 <fab.jaouen@gmail.com>
2021-06-04 10:29:19 +0200

    Change static folder location to pages app

--- advocacy_project/settings.py
+++ advocacy_project/settings.py
@@ -136,7 +136,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, '/pages/static/')
+
 
 # Needed to implement the account creation workflow.
 REGISTRATION_FORM = 'accounts.forms.CustomUserCreationForm'
