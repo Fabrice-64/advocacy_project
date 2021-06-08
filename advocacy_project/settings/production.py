74d30380661fdf084d5e037cc6cec6cb4dd8e160
Fabrice-64 <fab.jaouen@gmail.com>
2021-06-08 20:35:19 +0200

    activation sendgrid

--- advocacy_project/settings/production.py
+++ advocacy_project/settings/production.py
@@ -1,7 +1,16 @@
 from .base import *
 
 DEBUG = False
-
+ALLOWED_HOSTS = ['*']
 ADMINS = (
     ('Fabrice Jaouën', 'fabricejaouen@yahoo.com')
 )
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+SENDGRID_API_KEY = os.getenv('SENDGRID_API_KEY')
+EMAIL_HOST = 'smtp.sendgrid.net'
+EMAIL_HOST_USER = 'apikey'
+EMAIL_HOST_PASSWORD = SENDGRID_API_KEY
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+DEFAULT_FROM_EMAIL = "admin@advocacy-project.fr"
