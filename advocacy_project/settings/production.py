c1424bdf2e7e6fe73ea7942ba119e4e93bb60a55
Fabrice-64 <fab.jaouen@gmail.com>
2021-06-12 09:37:33 +0200

    amend settings for sendgrid

--- advocacy_project/settings/production.py
+++ advocacy_project/settings/production.py
@@ -2,3 +2,11 @@ from .base import *
 
 DEBUG = False
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+SENDGRID_API_KEY = os.getenv('SENDGRID_API_KEY')
+EMAIL_HOST = 'smtp.sendgrid.net'
+EMAIL_HOST_USER = 'apikey'
+EMAIL_HOST_PASSWORD = SENDGRID_API_KEY
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+DEFAULT_FROM_EMAIL = "admin@advocacy-project.fr"
